07fcdebdda644334dd87904b38ff38085d72b997
Daniel Paul Searles <daniel.paul.searles@gmail.com>
2019-04-16 06:01:26 -0700

    Add script for setting up helm.

---
+++ helm-init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+helm init
+
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
